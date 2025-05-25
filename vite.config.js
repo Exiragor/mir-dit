16b3f3456434f675d17917fc7b0048e8ee3b3610
Exiragor <exiragor@yandex.ru>
2025-05-25 15:40:32 +0300

    feat: add pages & start working on tablet size

--- vite.config.js
+++ vite.config.js
@@ -25,6 +25,96 @@ export default defineConfig({
             },
           },
         },
+        {
+          entry: "src/main.js",
+          filename: "about.html",
+          template: "about.html",
+          injectOptions: {
+            data: {
+              title: "Мир данных ИТ",
+              headerNav: fs.readFileSync("./src/components/header-nav.html"),
+              footer: fs.readFileSync("./src/components/footer.html"),
+              form: fs.readFileSync("./src/components/form.html"),
+              formBig: fs.readFileSync("./src/components/form-big.html"),
+            },
+          },
+          
+        },
+        {
+          entry: "src/main.js",
+          filename: "admin.html",
+          template: "admin.html",
+          injectOptions: {
+            data: {
+              title: "Мир данных ИТ",
+              headerNav: fs.readFileSync("./src/components/header-nav.html"),
+              footer: fs.readFileSync("./src/components/footer.html"),
+              form: fs.readFileSync("./src/components/form.html"),
+              formBig: fs.readFileSync("./src/components/form-big.html"),
+            },
+          },
+          
+        },
+        {
+          entry: "src/main.js",
+          filename: "analytic.html",
+          template: "analytic.html",
+          injectOptions: {
+            data: {
+              title: "Мир данных ИТ",
+              headerNav: fs.readFileSync("./src/components/header-nav.html"),
+              footer: fs.readFileSync("./src/components/footer.html"),
+              form: fs.readFileSync("./src/components/form.html"),
+              formBig: fs.readFileSync("./src/components/form-big.html"),
+            },
+          },
+          
+        },
+        {
+          entry: "src/main.js",
+          filename: "contacts.html",
+          template: "contacts.html",
+          injectOptions: {
+            data: {
+              title: "Мир данных ИТ",
+              headerNav: fs.readFileSync("./src/components/header-nav.html"),
+              footer: fs.readFileSync("./src/components/footer.html"),
+              form: fs.readFileSync("./src/components/form.html"),
+              formBig: fs.readFileSync("./src/components/form-big.html"),
+            },
+          },
+          
+        },
+        {
+          entry: "src/main.js",
+          filename: "oracle.html",
+          template: "oracle.html",
+          injectOptions: {
+            data: {
+              title: "Мир данных ИТ",
+              headerNav: fs.readFileSync("./src/components/header-nav.html"),
+              footer: fs.readFileSync("./src/components/footer.html"),
+              form: fs.readFileSync("./src/components/form.html"),
+              formBig: fs.readFileSync("./src/components/form-big.html"),
+            },
+          },
+          
+        },
+        {
+          entry: "src/main.js",
+          filename: "security.html",
+          template: "security.html",
+          injectOptions: {
+            data: {
+              title: "Мир данных ИТ",
+              headerNav: fs.readFileSync("./src/components/header-nav.html"),
+              footer: fs.readFileSync("./src/components/footer.html"),
+              form: fs.readFileSync("./src/components/form.html"),
+              formBig: fs.readFileSync("./src/components/form-big.html"),
+            },
+          },
+          
+        },
       ],
     }),
   ],
